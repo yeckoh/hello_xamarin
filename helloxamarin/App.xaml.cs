509855d277bb46c8e0a18401db24e099d67bbbe5
yeckoh <yeckoh@gmail.com>
2021-09-08 22:06:32 -0400

    simplify project files, move anonymous function into OnStart

--- helloxamarin/App.xaml.cs
+++ helloxamarin/App.xaml.cs
@@ -1,6 +1,5 @@
-﻿using helloxamarin.Services;
-using helloxamarin.Views;
-using System;
+﻿using System;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -8,17 +7,35 @@ namespace helloxamarin
 {
     public partial class App : Application
     {
+        //private static readonly Uri _TARGET_URL = new Uri("https://wikipedia.org");
+        private static readonly Uri _TARGET_URL = new Uri("https://durehjournal.com");
+        private static readonly BrowserLaunchOptions _BROWSER_OPTIONS = new BrowserLaunchOptions
+        {
+            LaunchMode = BrowserLaunchMode.External, // .SystemPreferred will launch within the app which may be preferable?
+            TitleMode = BrowserTitleMode.Default
+        };
 
         public App()
         {
             InitializeComponent();
 
-            DependencyService.Register<MockDataStore>();
-            MainPage = new AppShell();
+            MainPage = new MainPage();
         }
-
         protected override void OnStart()
         {
+            try
+            {
+                Browser.OpenAsync(_TARGET_URL, _BROWSER_OPTIONS);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.StackTrace.ToString());
+                // An unexpected error occured. No browser may be installed on the device.
+            }
+            finally
+            {
+                Quit(); // this does nothing
+            }
         }
 
         protected override void OnSleep()
